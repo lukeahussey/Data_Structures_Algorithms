534b659607284cf1496ac05ddbb2b37c0b04cd72
elhussler <animanic@hotmail.co.uk>
2024-11-25 16:46:16 +0000

    Add function for efficiently finding the first recurring character using a Set to store data, resulting in improved O(n) complexity

--- RecurringCharacterFinder.js
+++ RecurringCharacterFinder.js
@@ -15,9 +15,10 @@ class RecurringCharacterFinder {
     }
   }
 
-  // BRUTE-FORCE APPROACH - uses nested for loop to compare every array element to every other element
+  // BRUTE-FORCE APPROACH - nested for loop compares every array element to every other element
   firstRecurringCharacterBruteForce() {
-    let firstRecurringIndex = undefined;                  // O(1) space complexity
+    let firstRecurringIndex = undefined;                        // O(1) space complexity
+
     for (let i = 0; i < this._characters.length - 1; i++) {
       for (let j = i + 1; j < this._characters.length; j++) {   // O(n^2) time complexity
         if (this._characters[i] === this._characters[j]) {
@@ -28,8 +29,28 @@ class RecurringCharacterFinder {
       }
     }
     console.log('Characters array: ', this._characters, 
+      '\nFirst recurring character: ', this._characters[firstRecurringIndex],
+      '\nRecurring character index: ', firstRecurringIndex === undefined ? 'N/A' : firstRecurringIndex, '\n');
+
+    return this._characters[firstRecurringIndex];
+  }
+
+  // EFFICIENT APPROACH - stores encountered values in a JS Set, returning the current value if it is already in there
+  firstRecurringCharacterEfficient() {
+    const charactersSeen = new Set();                             // O(n) space complexity
+    let firstRecurringIndex = undefined;
+
+    for (let i = 0; i < this._characters.length; i++) {           // O(n) time complexity
+      if (charactersSeen.has(this._characters[i])) {
+        firstRecurringIndex = i;
+        break;
+      } else {
+        charactersSeen.add(this._characters[i]);
+      }
+    }
+    console.log('Characters array: ', this._characters, 
       '\nFirst recurring character: ', this._characters[firstRecurringIndex], 
-      '\nRecurring character index: ', firstRecurringIndex, '\n');
+      '\nRecurring character index: ', firstRecurringIndex === undefined ? 'N/A' : firstRecurringIndex, '\n');
 
     return this._characters[firstRecurringIndex];
   }
@@ -38,10 +59,13 @@ class RecurringCharacterFinder {
 let finder = new RecurringCharacterFinder();
 
 finder.characters = [2,5,1,2,3,5,1,2,4];
-const result = finder.firstRecurringCharacterBruteForce();
+const result1a = finder.firstRecurringCharacterBruteForce();
+const result1b = finder.firstRecurringCharacterEfficient();
 
 finder.characters = [2,1,1,2,3,5,1,2,4];
-const result2 = finder.firstRecurringCharacterBruteForce();
+const result2a = finder.firstRecurringCharacterBruteForce();
+const result2b = finder.firstRecurringCharacterEfficient();
 
 finder.characters = [2,3,4,5];
-const result3 = finder.firstRecurringCharacterBruteForce();
+const result3a = finder.firstRecurringCharacterBruteForce();
+const result3b = finder.firstRecurringCharacterEfficient();
