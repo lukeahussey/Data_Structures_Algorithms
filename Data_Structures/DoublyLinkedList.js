048a0af8e10d028e76b263c73a41d059e490bb90
elhussler <animanic@hotmail.co.uk>
2024-11-28 18:46:58 +0000

    Implemented Insert/Remove for doubly linked list, Noted future time complexity improvement due to prev pointers, Added clearer logging

--- Data_Structures/DoublyLinkedList.js
+++ Data_Structures/DoublyLinkedList.js
@@ -8,11 +8,13 @@ class Node {
 
 class DoublyLinkedList {
   constructor(value) {
+    console.log("\nCREATE LIST with value: ", value);
     this.head = new Node(value);
     this.tail = this.head;
     this.length = 1;
   }
   append(value) {
+    console.log("\nAPPEND value: ", value);
     const newTail = new Node(value);
     if (this.length === 0) {
       this.head = newTail;
@@ -26,6 +28,7 @@ class DoublyLinkedList {
     return this.printList();
   }
   prepend(value) {
+    console.log("\nPREPEND value: ", value);
     const newHead = new Node(value);
     newHead.next = this.head;
     this.head.prev = newHead;
@@ -34,24 +37,40 @@ class DoublyLinkedList {
     return this.printList();
   }
   insert(index, value) {
+    console.log("\nINSERT value: ", value, " at index: ", index);
     if (index === 0) {
       return this.prepend(value);
     }
     if (index >= this.length+1) {
       return this.append(value);
-    }    
+    }
+    // // POTENTIAL PERFORMANCE IMPROVEMENT
+    // if (index >= this.length / 2) {
+    //   // TRAVERSE BACKWARDS FROM TAIL      
+    // }
+    // else {
+    //   // TRAVERSE FORWARDS FROM HEAD
+    // }
     const newNode = new Node(value);
-    const leaderNode = this.traverseToIndex(index-1); // FIND the node before the insert index
-    newNode.next = leaderNode.next;
+    const leaderNode = this.traverseToIndex(index-1); // FIND the node AT the insert index
+    const followerNode = leaderNode.next;
     leaderNode.next = newNode;
+    newNode.prev = leaderNode;
+    newNode.next = followerNode;
+    followerNode.prev = newNode;
     this.length++;
     return this.printList();
   }
   remove(index) {
+    console.log("\nREMOVE index: ", index);
     if (index === 0) {
-      this.length > 1 
-        ? this.head = this.head.next
-        : this.head = null;
+      if (this.length > 1) {
+        this.head = this.head.next
+      }
+      else {
+        this.head = null;
+        this.tail = null;
+      }
     }
     else if (index >= this.length) {
       throw new Error(`Index "${index}" is invalid or outside the bounds of the list`);
@@ -59,13 +78,22 @@ class DoublyLinkedList {
     else {
       const leaderNode = this.traverseToIndex(index-1);
       const nodeToDelete = leaderNode.next;
-      leaderNode.next = nodeToDelete.next;
+      const followerNode = nodeToDelete.next;
+      if (followerNode === null) {
+        leaderNode.next = null;
+        this.tail = leaderNode;
+      }
+      else {
+        leaderNode.next = followerNode;
+        followerNode.prev = leaderNode;
+      }
       nodeToDelete.next = null;
+      nodeToDelete.prev = null;
     }
     this.length--;
     return this.printList();
   }
-  traverseToIndex(index) {
+  traverseToIndex(index, forwards) {
     if (isNaN(index) || index < 0 || index > this.length+1) {
       throw new Error(`Index "${index}" is invalid or outside the bounds of the list`);
     }
@@ -78,10 +106,12 @@ class DoublyLinkedList {
     return currentNode;
   }
   printList() {
+    console.log('\nthis.head: ', this.head, '\n', 'this.tail: ', this.tail);
     let currentNode = this.head;
-    let output = "HEAD: ";
+    let output = "\nLIST:\n";
     while (currentNode !== null) {
-      output += `${currentNode.value} <-> `;
+      output += 
+        `${currentNode === this.head ? '[HEAD]' : ''} (${currentNode.value}) ${currentNode === this.tail ? '[TAIL]' : ''} <-> `;
       currentNode = currentNode.next;
     }
     output += "NULL";
@@ -99,8 +129,8 @@ myLinkedList.append(16);
 myLinkedList.prepend(2);
 myLinkedList.prepend(1);
 
-// myLinkedList.insert(2,99);
-// myLinkedList.insert(4,96);
+myLinkedList.insert(2,99);
+myLinkedList.insert(4,96);
 
 myLinkedList.remove(2);
 myLinkedList.remove(4);
