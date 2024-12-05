6b558cfafa4f68eb1ae5c94c1ad5053d3fae603a
elhussler <animanic@hotmail.co.uk>
2024-12-05 16:09:00 +0000

    Simplify array implementation, removing duplicate manual length, top and bottom references

--- Data_Structures/Stack_Array.js
+++ Data_Structures/Stack_Array.js
@@ -1,53 +1,39 @@
 class Stack {
   constructor() {
     this.array = [];
-    this.top = null;
-    this.bottom = null;
-    this.length = 0;
   }
 
   peek() {
-    console.log("\nPEEK top node: ", this.top);
+    let top = this.array[this.array.length-1];
+    console.log("\nPEEK top node: ", top);
+    return top;
   }
 
   push(value) {
     console.log("\nPUSH value to top: ", value);
     this.array.push(value);
-    this.top = value;
-    if (this.length === 0) {
-      this.bottom = value;
-    }
-    this.length++;
     return this.printStack();
   }
 
   pop() {
-    console.log("\nPOP top value: ", this.top);
-    if (this.length <= 0) {
+    if (this.array.length <= 0) {
       console.log("Stack is empty, cannot pop");
       return null;
     }
-    this.array.pop();
-    this.length--;
-    if (this.length == 0) {
-      this.top = null;
-      this.bottom = null;
-    }
-    else {
-      this.top = this.array[this.length-1];
-    }
+    let top = this.array.pop();
+    console.log("\nPOP top value: ", top);
     return this.printStack();
   }
 
   isEmpty() {
-    console.log("\nStack is empty = ", this.length < 1);
+    console.log("\nStack is empty = ", this.array.length < 1);
     return this.length < 1;
   }
 
   printStack() {
-    console.log('\nthis.array: ', this.array, '\nthis.top: ', this.top, '\nthis.bottom: ', this.bottom, '\nthis.length: ', this.length);
+    console.log('\nthis.array: ', this.array);
     let output = "\nSTACK:\n";
-    for (let i = this.length-1; i >= 0; i--) {
+    for (let i = this.array.length-1; i >= 0; i--) {
       let val = this.array[i];
       output += `("${val}") ${val === this.top ? '[TOP] ' : ''}${val === this.bottom ? '[BOTTOM]' : ''}\n`;
     }
