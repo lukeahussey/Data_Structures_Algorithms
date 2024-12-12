9003a1e53eee4f7c1c5f2ae12dacc0af188625c2
elhussler <animanic@hotmail.co.uk>
2024-12-12 14:13:57 +0000

    recursive string reversal with complexities for both recursive and iterative approaches

---
+++ Algorithms/StringReverser_Recursive_Iterative.js
@@ -0,0 +1,25 @@
+function reverseString_Iterative(str) {                     // O(n^2) time complexity
+  console.log("str: " + str);                               // O(n) space complexity
+  let reversed = '';
+  for (let i = str.length-1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  console.log("reversed: " + reversed);
+}
+
+reverseString_Iterative("Hello my name is Luke");
+
+function reverseString_Recursive(str) {                     // O(n^2) time complexity
+  // base case                                              // O(n) space complexity
+  // if the input string has less than 2 characters then it is already "reversed"
+  if (str.length < 2) {
+    return str;
+  }
+  // recursive case
+  // reverse the substring starting at index 1 then add the character at index 0 to the end
+  return reverseString_Recursive(str.slice(1)) + str.charAt(0);
+}
+
+let str = "Hello my name is Luke";
+console.log("str: " + str);
+console.log("reversed: ", reverseString_Recursive(str));
