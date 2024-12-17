53f43b5dd765ca7df8ffdf8f8f65ffa646932c05
elhussler <animanic@hotmail.co.uk>
2024-12-17 15:23:23 +0000

    separate more readable nested for loop approach from alternative do while with nested for loop approach. both have the same complexity but do while can be less time efficient from iterating until no more swaps are possible

--- Algorithms/BubbleSort.js
+++ Algorithms/BubbleSort.js
@@ -1,7 +1,20 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+const numbers2 = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function bubbleSort(array) {                    // O(n^2) Time complexity
-  let swapped;                                  // O(1) Space complexity
+function bubbleSort(array) {                      // O(n^2) Time complexity
+  for (let i = 0; i < array.length; i++) {        // O(1) Space complexity
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] > array[j+1]) {
+        let holdingValue = array[j];
+        array[j] = array[j+1];
+        array[j+1] = holdingValue;
+      }
+    }
+  }
+}
+
+function bubbleSort_alt(array) {                  // O(n^2) Time complexity
+  let swapped;                                    // O(1) Space complexity
   do {
     swapped = false;
     for (let i = 1; i < array.length; i++) {
@@ -16,5 +29,10 @@ function bubbleSort(array) {                    // O(n^2) Time complexity
   while (swapped)
 }
 
+console.log("Unsorted: ", numbers);
 bubbleSort(numbers);
-console.log(numbers);
+console.log("Sorted: ", numbers);
+
+console.log("Unsorted: ", numbers2);
+bubbleSort_alt(numbers2);
+console.log("Sorted: ", numbers2);
