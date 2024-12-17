a378fedbc3dfb6ddb16e238a5476726421a11d4f
elhussler <animanic@hotmail.co.uk>
2024-12-17 17:38:12 +0000

    Alternate method using nested for loops with JS array manipulation methods, unshift is not performant on larger input sizes

--- Algorithms/InsertionSort.js
+++ Algorithms/InsertionSort.js
@@ -1,6 +1,7 @@
 const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+const numbers2 = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
 
-function insertionSort(array) {
+function insertionSort_NestedForAndWhileLoops(array) {
   for (let i = 1; i < array.length; i++) {
     let index = i;
     while (index > 0) {
@@ -16,6 +17,30 @@ function insertionSort(array) {
   }
 }
 
+function insertionSort_NestedForLoops(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < array[0]) {
+      // Move number to array index 0
+      array.unshift(array.splice(i, 1)[0]);
+    } else {
+      // Find correct number location
+      for (let j = 1; j < i; j++) {
+        if (array[i] > array[j - 1] && array[i] < array[j]) {
+          // Move number to correct index
+          array.splice(j, 0, array.splice(i, 1)[0]);
+        }
+      }
+    }
+    console.log("numbers: ", numbers2);
+  }
+
+  return array;
+}
+
 console.log("Unsorted: ", numbers, "\n");
-insertionSort(numbers);
+insertionSort_NestedForAndWhileLoops(numbers);
 console.log("\nSorted:   ", numbers);
+
+console.log("\n\nUnsorted: ", numbers2, "\n");
+insertionSort_NestedForLoops(numbers2);
+console.log("\nSorted:   ", numbers2);
