6fb01dd63b1289ce1713df9c32b9ae0c921a8d2d
elhussler <animanic@hotmail.co.uk>
2024-12-20 15:28:34 +0000

    finished breadth first search outputting nodes in BFS order

--- Algorithms/BreadthFirstSearch_BinarySearchTree.js
+++ Algorithms/BreadthFirstSearch_BinarySearchTree.js
@@ -114,7 +114,22 @@ class BinarySearchTree {
     }
   }
   breadthFirstSearch() {
-    
+    let currentNode = this.root;
+    let list = [];
+    let queue = [];
+    queue.push(currentNode);
+
+    while (queue.length > 0) {
+      currentNode = queue.shift();
+      console.log(currentNode.value);
+      list.push(currentNode.value);
+      if (currentNode.left) {
+        queue.push(currentNode.left);
+      }
+      if (currentNode.right) {
+        queue.push(currentNode.right);
+      }
+    }
   }
 }
 
