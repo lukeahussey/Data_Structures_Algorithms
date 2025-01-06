88de59c3728fe983f354352327948244bd876fca
elhussler <animanic@hotmail.co.uk>
2025-01-06 16:27:14 +0000

    Refactor using JS Scoping to reduce cache from global to function scope

--- Algorithms/DynamicProgramming_Memoisation.js
+++ Algorithms/DynamicProgramming_Memoisation.js
@@ -1,16 +1,35 @@
-let cache = {};
-
-function memoizeAddTo80(n) {
-  if (n in cache) {
-    console.log('BYPASSING long, complex operation using memoization');
-    return cache[n];
-  } else {
-    console.log('long, complex operation');
-    const answer = n + 80;
-    cache[n] = answer;
-    return answer;
+function memoizeAddTo9000(n) { 
+  let cache = {};                     // Cache is now scoped not global
+  return function(n) {                // SCOPING - returns function with access to cache
+    if (n in cache) {
+      console.log('BYPASSING long, complex operation using memoization');
+      return cache[n];
+    } else {
+      console.log('long, complex operation');
+      const answer = n + 9000;
+      cache[n] = answer;
+      return answer;
+    }
   }
 }
 
-console.log(1, memoizeAddTo80(6))
-console.log(2, memoizeAddTo80(6))
+const memoized = memoizeAddTo9000();
+console.log(1, memoized(6))
+console.log(2, memoized(6))
+
+// let cache = {};
+
+// function memoizeAddTo9000(n) {
+//   if (n in cache) {
+//     console.log('BYPASSING long, complex operation using memoization');
+//     return cache[n];
+//   } else {
+//     console.log('long, complex operation');
+//     const answer = n + 9000;
+//     cache[n] = answer;
+//     return answer;
+//   }
+// }
+
+// console.log(1, memoizeAddTo9000(6))
+// console.log(2, memoizeAddTo9000(6))
