15b81cc3c7b9d77376f6ac9aa8bb2bd4450edebd
elhussler <animanic@hotmail.co.uk>
2025-01-06 17:01:55 +0000

    dynamic programming memoization exponentially reduces number of calculations

--- Algorithms/DynamicProgramming_Fibonacci.js
+++ Algorithms/DynamicProgramming_Fibonacci.js
@@ -1,13 +1,37 @@
 let nonDynamicCalculations = 0;
 
-function fibonacci(n) {   //O(2^n) - Exponential Time Complexity
+function fibonacci(n) {                   //O(2^n) - Exponential Time Complexity
   nonDynamicCalculations++;
-  if (n < 2) {            // Base case
+  // Base case
+  if (n < 2) {
     return n;
   }
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-fibonacci(10);
-console.log('nonDynamicCalculations: ', nonDynamicCalculations);
 
+let dynamicCalculations = 0;
+function fibonacciDynamicProgramming() {  //O(n) - Linear Time Complexity
+  let cache = {};
+  return function fib(n) {
+    dynamicCalculations++;
+    // Subproblem already solved
+    if (n in cache) {
+      return cache[n];
+    } else {
+      // Base case
+      if (n < 2) {
+        return n;
+      } else {
+        cache[n] = fib(n - 1) + fib(n - 2);
+        return cache[n];
+      }
+    }
+  }
+}
+
+console.log('Non-DP', fibonacci(10));
+console.log('nonDynamicCalculations: ', nonDynamicCalculations);
+const dynamicFibonacci = fibonacciDynamicProgramming();
+console.log('DP', dynamicFibonacci(10));
+console.log('dynamicCalculations: ', dynamicCalculations);
