e8e948d217bacd0a0c7a5d178c77eeb9ce85d55a
elhussler <animanic@hotmail.co.uk>
2025-01-07 14:47:27 +0000

    additional comments to emphasise number of repeated calculations

--- Algorithms/DynamicProgramming_Fibonacci.js
+++ Algorithms/DynamicProgramming_Fibonacci.js
@@ -2,15 +2,18 @@ let nonDynamicCalculations = 0;
 
 function fibonacci(n) {                   //O(2^n) - Exponential Time Complexity
   nonDynamicCalculations++;
+  console.log('Calculating fibonacci of', n);
   // Base case
   if (n < 2) {
     return n;
   }
+  console.log('Calculating fibonacci of', n - 1, 'and', n - 2); // This is the recursive call
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
 
 let dynamicCalculations = 0;
+
 function fibonacciDynamicProgramming() {  //O(n) - Linear Time Complexity
   let cache = {};
   return function fib(n) {
