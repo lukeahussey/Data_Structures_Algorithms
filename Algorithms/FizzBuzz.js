73f606dcbc664abf31d7bbb67475c60174ec211b
elhussler <animanic@hotmail.co.uk>
2025-01-15 17:23:51 +0000

    FizzBuzz algorithm using for loop and stringbuilding to append Fizz and/or Buzz or the number to an array of numbers 1-n

---
+++ Algorithms/FizzBuzz.js
@@ -0,0 +1,19 @@
+var fizzBuzz = function(n) {
+  let arr = [];                           //O(n) space complexity
+  for (let i = 1; i <= n; i++) {          //O(n) time complexity
+      let tempString = "";
+      if (i % 3 === 0) {
+          tempString += "Fizz";
+      }
+      if (i % 5 === 0) {
+          tempString += "Buzz";
+      }
+      if (tempString === "") {
+          tempString += i;
+      }
+      arr[i-1] = tempString;
+  }
+  return arr;
+}
+
+console.log(fizzBuzz(15)); //["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
