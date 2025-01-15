ba41133872ea28aadd0955abd108000aad90df0c
elhussler <animanic@hotmail.co.uk>
2025-01-15 17:24:27 +0000

    Add input validation

--- Algorithms/FizzBuzz.js
+++ Algorithms/FizzBuzz.js
@@ -1,4 +1,11 @@
 var fizzBuzz = function(n) {
+  if (n < 1 || n > 100000) {
+      throw new Error("Input outside of accepted range");
+  }
+  if (isNaN(n)) {
+      throw new Error("Input is not a valid number");
+  }
+  
   let arr = [];                           //O(n) space complexity
   for (let i = 1; i <= n; i++) {          //O(n) time complexity
       let tempString = "";
@@ -16,4 +23,4 @@ var fizzBuzz = function(n) {
   return arr;
 }
 
-console.log(fizzBuzz(15)); //["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
+console.log(fizzBuzz(15)); // ["1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz"]
