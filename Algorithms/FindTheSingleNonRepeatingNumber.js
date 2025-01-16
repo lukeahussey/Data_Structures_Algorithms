786281f56283e1a5a4f087296742609cdec1ba0f
elhussler <animanic@hotmail.co.uk>
2025-01-16 16:17:15 +0000

    add tests and comments indicating complexity

--- Algorithms/FindTheSingleNonRepeatingNumber.js
+++ Algorithms/FindTheSingleNonRepeatingNumber.js
@@ -1,9 +1,10 @@
 var singleNumber = function(nums) {
+    // Brute-force/Naive approach: compare every element with every other element to see if it reoccurs
     for (let i = 0; i < nums.length; i++) {
         let num = nums[i];
         let matchFound = false;
-        for (let j = 0; j < nums.length; j++) {
-            if (i !== j) {
+        for (let j = 0; j < nums.length; j++) {             // O(n^2) time complexity
+            if (i !== j) {                                  // O(1) space complexity
                 if (nums[j] === num) {
                     matchFound = true;
                     break;
@@ -15,3 +16,7 @@ var singleNumber = function(nums) {
         }
     }
 };
+
+console.log(singleNumber([2,2,1]));          // 1
+console.log(singleNumber([4,1,2,1,2]));      // 4
+console.log(singleNumber([1]));              // 1
