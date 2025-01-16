ed79e92c72138a21925d011a661f7e68dd80558e
elhussler <animanic@hotmail.co.uk>
2025-01-16 16:52:02 +0000

    More time efficient approach using hashmap to store frequency of each number with Linear time and space complexity

--- Algorithms/FindTheSingleNonRepeatingNumber.js
+++ Algorithms/FindTheSingleNonRepeatingNumber.js
@@ -1,5 +1,5 @@
+// Brute-force/Naive approach: compare every element with every other element to see if it reoccurs
 var singleNumber = function(nums) {
-    // Brute-force/Naive approach: compare every element with every other element to see if it reoccurs
     for (let i = 0; i < nums.length; i++) {
         let num = nums[i];
         let matchFound = false;
@@ -20,3 +20,23 @@ var singleNumber = function(nums) {
 console.log(singleNumber([2,2,1]));          // 1
 console.log(singleNumber([4,1,2,1,2]));      // 4
 console.log(singleNumber([1]));              // 1
+
+
+// Efficient approach: Linear space and Linear time
+var singleNumberEfficient = function(nums) {
+  const countMap = new Map();                             // O(n) space complexity
+
+  for (const num of nums) {
+      countMap.set(num, (countMap.get(num) || 0) + 1);    // O(n) time complexity
+  }
+
+  for (const [num, count] of countMap.entries()) {        // O(n) time complexity
+      if (count === 1) {
+          return num;
+      }
+  }
+};
+
+console.log(singleNumberEfficient([2,2,1]));          // 1
+console.log(singleNumberEfficient([4,1,2,1,2]));      // 4
+console.log(singleNumberEfficient([1]));              // 1
